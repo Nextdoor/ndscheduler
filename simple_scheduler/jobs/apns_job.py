809c81e847cf2a4339c1b17a38f706a6a1a1418e
Abhijeet Kumar <abhijeet@nextdoor.com>
2016-02-03 10:55:06 -0800

    review comments

--- simple_scheduler/jobs/apns_job.py
+++ simple_scheduler/jobs/apns_job.py
@@ -1,6 +1,7 @@
 """Simple job to send test Apple Push Notifications."""
 
 import logging
+import os
 
 from ndscheduler import job
 from apns import APNs, Payload
@@ -9,13 +10,14 @@ logger = logging.getLogger(__name__)
 
 
 class APNSJob(job.JobBase):
-    TIMEOUT = 10
 
     @classmethod
     def meta_info(cls):
         return {
             'job_class_string': '%s.%s' % (cls.__module__, cls.__name__),
-            'notes': 'This sends a push notification to APNS servers',
+            'notes': 'This sends a push notification to APNS servers. '
+                     'The environment variable APNS_CERT_PATH" should be provided '
+                     'for APNS cert file path.',
             'arguments': [
                 # APNS device token
                 {'token': 'string', 'description': 'Device token'},
@@ -28,7 +30,8 @@ class APNSJob(job.JobBase):
     def run(self, token, alert="Hello World",  *args, **kwargs):
         print 'Sending %s to %s' % (alert, token)
 
-        apns = APNs(use_sandbox=False, cert_file='simple_scheduler/jobs/apns-cert.pem')
+        cert_file = os.environ['APNS_CERT_PATH'] or 'simple_scheduler/jobs/apns-cert.pem'
+        apns = APNs(use_sandbox=False, cert_file=cert_file)
         # Send a notification
         payload = Payload(alert=alert, sound="default", badge=0)
         apns.gateway_server.send_notification(token, payload)

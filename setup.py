13b8b59a1b94e7e1ead5e6b5a072e6117c79dc1e
Matt Terry <mrterry@nextdoor.com>
2018-11-27 13:54:46 -0800

    ndscheduler does not work with py 3.7

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ classes = """
     Programming Language :: Python :: 3.4
     Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
-    Programming Language :: Python :: 3.7
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: OS Independent
 """

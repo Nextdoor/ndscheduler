7649f00c17371e38961bf9417b76fe5c02359323
Fake-Name <something@fake-url.com>
2019-05-18 00:48:41 -0700

    Support pypy postgresql.

--- ndscheduler/core/datastore/providers/postgresql.py
+++ ndscheduler/core/datastore/providers/postgresql.py
@@ -1,5 +1,6 @@
 """Represents PostgreSQL datastore."""
 
+import sys
 from ndscheduler import settings
 from ndscheduler.core.datastore.providers import base
 
@@ -22,10 +23,19 @@ class DatastorePostgresql(base.DatastoreBase):
         :rtype: str
         """
 
-        return 'postgresql://%s:%s@%s:%d/%s?sslmode=%s' % (
-            settings.DATABASE_CONFIG_DICT['user'],
-            settings.DATABASE_CONFIG_DICT['password'],
-            settings.DATABASE_CONFIG_DICT['hostname'],
-            settings.DATABASE_CONFIG_DICT['port'],
-            settings.DATABASE_CONFIG_DICT['database'],
-            settings.DATABASE_CONFIG_DICT['sslmode'])
+        # Work under Pypy, which doesn't have the default psycopg2
+        if '__pypy__' in sys.builtin_module_names:
+            db_wrapper = 'postgresql+psycopg2cffi'
+        else:
+            db_wrapper = 'postgresql'
+
+
+        return '%s://%s:%s@%s:%d/%s?sslmode=%s' % (
+                db_wrapper,
+                settings.DATABASE_CONFIG_DICT['user'],
+                settings.DATABASE_CONFIG_DICT['password'],
+                settings.DATABASE_CONFIG_DICT['hostname'],
+                settings.DATABASE_CONFIG_DICT['port'],
+                settings.DATABASE_CONFIG_DICT['database'],
+                settings.DATABASE_CONFIG_DICT['sslmode']
+            )

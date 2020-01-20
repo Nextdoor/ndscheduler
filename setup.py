769ea5362442ed5609770895a1aab346e17e7aae
Martin <martin@mnprojects.net>
2020-01-20 11:12:32 +0100

    Remove simple_scheduler from beeing installed.
    Exclude simple_scheduler package from setup.py.

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     download_url='http://pypi.python.org/pypi/ndscheduler#downloads',
     license='Apache License, Version 2',
     keywords='scheduler nextdoor cron python',
-    packages=find_packages(),
+    packages=find_packages(exclude=("simple_scheduler*",)),
     include_package_data=True,
     extras_require={'python_version<"3.3"': ['funcsigs']},
     tests_require=[
